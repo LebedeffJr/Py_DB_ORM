85ddf5a91806fa5e706f08258643caa482a1d096
LebedeffJr <Lebedeffjr@gmail.com>
2023-05-25 16:31:46 +0300

    Assignment 2 is done

--- Py_db_ORM_HW.py
+++ Py_db_ORM_HW.py
@@ -1,17 +1,19 @@
 import sqlalchemy as sq
+from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 import json
+import pprint
 
 DB_tipe = "postgresql"
 login = "postgres"
-password = "___"
+password = "______"
 host = "localhost"
 port = "5432"
 DB_name = "BooksDB"
 
 DSN = f'{DB_tipe}://{login}:{password}@{host}:{port}/{DB_name}'
 engine = sq.create_engine(DSN)
-
+conn = engine.connect()
 Base = declarative_base()
 
 class Publisher(Base):
@@ -20,6 +22,8 @@ class Publisher(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
 
+    def __str__(self):
+        return self.name
 class Book(Base):
     __tablename__ = "book"
 
@@ -28,12 +32,17 @@ class Book(Base):
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
 
     publisher = relationship(Publisher, backref="books")
+    def __str__(self):
+        return self.title
 
 class Shop(Base):
     __tablename__ = "shop"
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Stock(Base):
     __tablename__ = "stock"
@@ -56,9 +65,11 @@ class Sale(Base):
     count = sq.Column(sq.Integer)
 
     stock = relationship(Stock, backref="sales")
+    def __str__(self):
+        return self.price, self.date_sale
 
 def create_tables(engine):
-    Base.metadata.drop_all(engine)
+    #Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
 create_tables(engine)
@@ -100,8 +111,25 @@ def add_data(json_data):
                         )
             session.add(sale)
 
+def get_info(publisher_name):
+    info = session.query(Publisher.name, Book.title, Shop.name, Sale.price, Sale.date_sale
+                         ).join(Book).join(Stock).join(Shop).join(Sale
+                         ).filter(Publisher.name == publisher_name).all()
+    return info
+
+
+
 Session = sessionmaker(bind=engine)
 session = Session()
-add_data(data)
+#add_data(data)
+
+pub_name = input("Введите имя издателя: ")
+
+info = get_info(pub_name)
+for i in info:
+    print(f"{i[1]}".ljust(40)
+          +f"| {i[2]}".ljust(10)
+          +f" | {i[3]}".ljust(9)
+          + f"| {i[4]}")
 session.commit()
 session.close()
